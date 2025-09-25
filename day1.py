38207b3f8e8736848985305d6264bddac39d0dbf
Student ApnaCollege <payalrawat1205@gmail.com>
2025-09-25 18:23:09 +0530

    add file

--- day1.py
+++ day1.py
@@ -1,25 +1,35 @@
-# Google Form Simulation with Different Print Methods
+'''task1:write code for
+  1.Numeric:integer(positive,negative,binary,octal,hexa)
+  2.float
+  3.complex
+  4.Boolean'''
 
-# Taking user input (different data types)
-name = input("Enter your name: ")            # string
-age = int(input("Enter your age: "))         # integer
-height = float(input("Enter your height (in cm): "))   # float
-student = input("Are you a student? (yes/no): ")       # string
-marks = float(input("Enter your marks (out of 100): "))  # float
+#1.Numeric:integer(positive,negative,binary,octal,hexa)
+pos_int=62;
+neg_int=-45;
+bol_int=0b1010;
+oct_int=0o52;
+hex_int=0x2A;
 
-print("\n--- Form Submitted Successfully! ---")
+print("positive integer",pos_int)
+print("negative integer",neg_int)
+print("boolean integer",bol_int)
+print("octal integer",oct_int)
+print("hexadecimal integer",hex_int)
 
-# 1. Using Comma
-print("Name:", name, ", Age:", age, ", Height:", height, ", Student:", student, ", Marks:", marks)
+#2.float
+float_num=45.236
+print("float number",float_num)
 
-# 2. Using Plus (+)
-print("Name: " + name + " | Age: " + str(age) + " | Height: " + str(height) + " | Student: " + student + " | Marks: " + str(marks))
+#complex
+comp_num=5+5j;
+print("complex numer:",comp_num);
 
-# 3. Using Percent (%)
-print("Name: %s | Age: %d | Height: %.2f | Student: %s | Marks: %.1f" % (name, age, height, student, marks))
+#boolean
+bool_true = True
+bool_false = False
+print("Boolean True:", bool_true)
+print("Boolean False:", bool_false)
 
-# 4. Using format()
-print("Name: {} | Age: {} | Height: {:.2f} | Student: {} | Marks: {:.1f}".format(name, age, height, student, marks))
 
-# 5. Using f-string
-print(f"Name: {name} | Age: {age} | Height: {height:.2f} | Student: {student} | Marks: {marks:.1f}")
+
